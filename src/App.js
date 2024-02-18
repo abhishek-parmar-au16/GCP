fe86c3ec6c536c63ca2231390fc95f1cadb718c8
abhishek-parmar-au16 <abhishekparmar220197@gmail.com>
2024-02-18 18:48:43 +0530

    API Integration

--- src/App.js
+++ src/App.js
@@ -28,11 +28,14 @@ const dummyObjectStorage = {
 // Project list component
 const ProjectList = ({ projects, onSelectProject }) => {
   return (
-    <div>
+    <div style={{padding:"1rem"}}>
       <h2>Projects</h2>
       <ul>
         {projects.map((project) => (
-          <li key={project.id} onClick={() => onSelectProject(project)}>
+          <li
+            key={project.name}
+            onClick={() => onSelectProject(project?.services)}
+          >
             {project.name}
           </li>
         ))}
@@ -42,10 +45,16 @@ const ProjectList = ({ projects, onSelectProject }) => {
 };
 
 // Project details component
-const ProjectDetails = ({ project }) => {
+const ProjectDetails = ({ project, onSelectServices }) => {
   return (
-    <div>
-      <h2>Project Details - {project.name}</h2>
+    <div style={{padding:"1rem"}}>
+      <h2 style={{color:"blue"}}>Services List</h2>
+
+      {project.map((projecct) => (
+        <li key={projecct.name} onClick={() => onSelectServices(projecct)}>
+          {projecct.name}
+        </li>
+      ))}
       {/* Add links to service dashboards */}
     </div>
   );
@@ -53,21 +62,34 @@ const ProjectDetails = ({ project }) => {
 
 // Server Instances dashboard component
 const ServerInstancesDashboard = ({ serverInstances }) => {
+  let serviceKey = Object.keys(serverInstances?.specs);
+  let serviceValues = Object.values(serverInstances?.specs);
+  console.log("ran", serviceKey);
+  console.log("val", serviceValues);
   return (
-    <div>
-      <h2>Server Instances Dashboard</h2>
+    <div style={{padding:"1rem"}}>
+      <h2 style={{color:"blue"}}>CPU Dashboard</h2>
       <div
-              style={{
-                color: "grey",
-                border: "1px solid grey",
-                borderRadius: "10px",
-                padding: "10px",
-              }}
-            >
-              <h3>Billing</h3>
-              <p>Your billing for this month</p>
-              <h5>$78.9</h5>
-            </div>
+        style={{
+          color: "grey",
+          border: "1px solid grey",
+          borderRadius: "10px",
+          padding: "10px",
+        }}
+      >
+        <div style={{ display: "flex" }}>
+          <div>
+            {serviceKey.map((idx) => (
+              <h3>{idx} {" "}</h3>
+            ))}
+          </div>
+          <div>
+            {serviceValues.map((idx) => (
+              <h3>{" "}{idx}</h3>
+            ))}
+          </div>
+        </div>
+      </div>
       {/* Display graphs, charts, and controls for server instances */}
     </div>
   );
@@ -75,61 +97,56 @@ const ServerInstancesDashboard = ({ serverInstances }) => {
 
 // Firewall dashboard component
 const FirewallDashboard = ({ firewallRules }) => {
+  const ObjectArr = firewallRules?.usage.map((idx) =>
+    idx?.objects ? idx?.objects : idx?.memory ? idx.memory : idx?.size
+  );
+  console.log("pyar", ObjectArr);
   return (
-    <div>
-      <h2>Request Per Seconds</h2>
+    <div style={{padding:"1rem"}}>
+      <h2 style={{color:"blue"}}>CPU Usage</h2>
       <CChart
-                type="line"
-                data={{
-                  labels: [
-                    "1",
-                    "2",
-                    "3",
-                    "4",
-                    "5",
-                    "6",
-                    "7",
-                  ],
-                  datasets: [
-                    {
-                      label: "Request Per Seconds",
-                      backgroundColor: "rgba(220, 220, 220, 0.2)",
-                      borderColor: "rgba(165, 139, 143, 0.85)",
-                      pointBackgroundColor: "rgba(220, 220, 220, 1)",
-                      pointBorderColor: "#fff",
-                      data: [40, 20, 12, 39, 10, 40, 39, 80, 40],
-                    },
-                    
-                  ],
-                }}
-                // options={{
-                //   plugins: {
-                //     legend: {
-                //       labels: {
-                //         color: getStyle('--cui-body-color'),
-                //       }
-                //     }
-                //   },
-                //   scales: {
-                //     x: {
-                //       grid: {
-                //         color: getStyle('--cui-border-color-translucent'),
-                //       },
-                //       ticks: {
-                //         color: getStyle('--cui-body-color'),
-                //       },
-                //     },
-                //     y: {
-                //       grid: {
-                //         color: getStyle('--cui-border-color-translucent'),
-                //       },
-                //       ticks: {
-                //         color: getStyle('--cui-body-color'),
-                //       },
-                //     },
-                //   },
-                // }}
-              />
+        type="line"
+        data={{
+          labels: ["1", "2", "3", "4", "5", "6", "7"],
+          datasets: [
+            {
+              label: "Request Per Seconds",
+              backgroundColor: "rgba(220, 220, 220, 0.2)",
+              borderColor: "rgba(165, 139, 143, 0.85)",
+              pointBackgroundColor: "rgba(220, 220, 220, 1)",
+              pointBorderColor: "#fff",
+              data: ObjectArr,
+            },
+          ],
+        }}
+        // options={{
+        //   plugins: {
+        //     legend: {
+        //       labels: {
+        //         color: getStyle('--cui-body-color'),
+        //       }
+        //     }
+        //   },
+        //   scales: {
+        //     x: {
+        //       grid: {
+        //         color: getStyle('--cui-border-color-translucent'),
+        //       },
+        //       ticks: {
+        //         color: getStyle('--cui-body-color'),
+        //       },
+        //     },
+        //     y: {
+        //       grid: {
+        //         color: getStyle('--cui-border-color-translucent'),
+        //       },
+        //       ticks: {
+        //         color: getStyle('--cui-body-color'),
+        //       },
+        //     },
+        //   },
+        // }}
+      />
       {/* Display number of rules and list of rules */}
     </div>
   );
@@ -137,9 +154,27 @@ const FirewallDashboard = ({ firewallRules }) => {
 
 // Object Storage dashboard component
 const ObjectStorageDashboard = ({ objectStorage }) => {
+  console.log("okok",objectStorage);
   return (
-    <div>
-      <h2>Object Storage Dashboard</h2>
+    <div style={{padding:"1rem"}}>
+      <h2 style={{color:"blue"}}>Type and Slug Dashboard</h2>
+      <div
+        style={{
+          color: "grey",
+          border: "1px solid grey",
+          borderRadius: "10px",
+          padding: "10px",
+        }}
+      >
+        <div style={{ display: "flex" }}>
+          <div>
+              <h3>Slugs : {objectStorage?.slug}</h3>
+              <h3>Type :{objectStorage?.type}</h3>
+           
+          </div>
+        </div>
+      </div>
+
       {/* Display storage usage, free space, and number of hits */}
     </div>
   );
@@ -149,12 +184,25 @@ const ObjectStorageDashboard = ({ objectStorage }) => {
 const App = () => {
   const [loggedInUser, setLoggedInUser] = useState(null);
   const [selectedProject, setSelectedProject] = useState(null);
+  const [selectedservice, setSelectedservice] = useState(null);
+  const [apiData, setApiData] = useState([]);
 
   useEffect(() => {
+    const config = {
+      headers: { Authorization: `Bearer 8e7f39bb-a52c-4528-8202-1c76d3d9ba5f` },
+    };
+    const bodyParameters = {
+      key: "value",
+    };
     const called = axios
-      .get("https://servers.sanboxes.soulharsh007.dev")
+      .get(
+        "https://servers.sanboxes.soulharsh007.dev/api/projects",
+        config,
+        bodyParameters
+      )
       .then((res) => {
-        console.log("res", res);
+        console.log("res", res.data.data);
+        setApiData(res.data.data);
       });
   }, []);
   const handleLogin = (username) => {
@@ -162,7 +210,13 @@ const App = () => {
   };
 
   const handleSelectProject = (project) => {
+    console.log("servisce", project);
     setSelectedProject(project);
+  };
+
+  const handleSelectServices = (project) => {
+    console.log("servoce-- ", project);
+    setSelectedservice(project);
   };
 
   return (
@@ -171,27 +225,31 @@ const App = () => {
         <Login onLogin={handleLogin} />
       ) : (
         <div>
-          <h1>Welcome, {loggedInUser}!</h1>
-          {selectedProject ? (
-            <ProjectDetails project={selectedProject} />
-          ) : (
-            <ProjectList
-              projects={dummyProjects}
-              onSelectProject={handleSelectProject}
-            />
-          )}
-          
+          <h1 style={{color:"red"}}>Welcome, {loggedInUser}!</h1>
+          <div className="gulabzain" style={{ display: "flex" }}>
+            {selectedProject ? (
+              <ProjectDetails
+                project={selectedProject}
+                onSelectServices={handleSelectServices}
+              />
+            ) : (
+              <ProjectList
+                projects={apiData}
+                onSelectProject={handleSelectProject}
+              />
+            )}
 
-          {/* Add service dashboards */}
-          {selectedProject && (
-            <ServerInstancesDashboard serverInstances={dummyServerInstances} />
-          )}
-          {selectedProject && (
-            <FirewallDashboard firewallRules={dummyFirewallRules} />
-          )}
-          {selectedProject && (
-            <ObjectStorageDashboard objectStorage={dummyObjectStorage} />
-          )}
+            {/* Add service dashboards */}
+            {selectedservice && (
+              <ServerInstancesDashboard serverInstances={selectedservice} />
+            )}
+            {selectedservice && (
+              <FirewallDashboard firewallRules={selectedservice} />
+            )}
+            {selectedservice && (
+              <ObjectStorageDashboard objectStorage={selectedservice} />
+            )}
+          </div>
         </div>
       )}
     </div>
